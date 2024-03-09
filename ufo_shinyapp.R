8132075fa2c92d08ea6c9a8f96f72e19d3bc6de5
cadoria <cmdoria@ucdavis.edu>
2024-03-08 16:04:46 -0800

    ShinyApp update

---
+++ ufo_shinyapp.R
@@ -0,0 +1,80 @@
+library(shiny)
+library(shinythemes)
+library(shinydashboard)
+
+library("tidyverse")
+library("naniar")
+library("janitor")
+library("ggmap")
+library("dplyr")
+
+ufo <- readr::read_csv("ufo_data/complete.csv", na = c("", "unkown")) %>% clean_names
+
+ufo2 <- ufo %>%
+  separate(datetime, into = c("month", "day","year"), sep="/") %>%
+  separate(year, into = c("year", "time"), sep = " ") %>%
+  select(-duration_hours_min, -comments, -date_posted) %>%
+  filter(country=="us", latitude!=0, longitude!=0,  !is.na(latitude), !is.na(longitude)) %>%
+  mutate(year=as.factor(year)) %>%
+  mutate(latitude=as.numeric(latitude)) %>%
+  mutate(longitude=as.numeric(longitude)) %>%
+  mutate(shape=as.factor(shape)) %>%
+  mutate(state=as.factor(state))
+
+ui <- fluidPage(
+  theme = shinytheme("journal"),
+  # App title 
+  tags$head(
+    tags$style(
+      HTML("
+           .title {
+             font-family: 'Chewy', cursive;
+             font-size: 24px;
+           }
+           "
+      )
+    ),
+  ),
+  headerPanel(
+    div(class = "title", "UFO Tracker")
+  ),
+  
+  # Sidebar panel for inputs
+  sidebarPanel(
+    selectInput("shape", "Select UFO Shape", choices = c(unique(ufo2$shape))),
+    selectInput("state", "Select State", choices = c(unique(ufo2$state)))
+  ),
+  
+  # Main panel for displaying outputs 
+  mainPanel(
+    plotOutput("plot")
+  )
+)
+
+# Define server logic 
+server <- function(input, output) {
+  
+  # Render plot based on selected UFO shape and state
+  output$plot <- renderPlot({
+    req(input$shape, input$state)  # Require both inputs to be selected
+    
+    filtered_data <- ufo2 %>%
+      filter(shape == input$shape & state == input$state)
+    
+    if (nrow(filtered_data) == 0) {
+      # If no data found, display a message
+      plot(1, type = "n", axes = FALSE, xlab = "", ylab = "", main = "No data available")
+    } else {
+      # Otherwise, plot the data
+      ggplot(filtered_data, aes(x = month)) +
+        geom_bar(fill = "skyblue") +
+        labs(title = paste("UFO Sightings for Shape:", input$shape, "and State:", input$state),
+             x = "Month",
+             y = "Number of Sightings") +
+        theme_linedraw()
+    }
+  })
+}
+
+# Start the app
+shinyApp(ui, server)
